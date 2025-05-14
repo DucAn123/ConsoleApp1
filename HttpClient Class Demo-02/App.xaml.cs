5401e27a14cd8608cca11ea10cdb3be4fa1db6c8
DucAn123 <anndha160101@fpt.edu.vn>
2025-05-14 14:35:22 +0700

    Add WPF project DemoHttpClient with HttpClient GET demo

---
+++ HttpClient Class Demo-02/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HttpClient_Class_Demo_02
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ HttpClient Class Demo-02/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http;
+using System.Windows;
+
+namespace DemoHttpClient
+{
+    public partial class MainWindow : Window
+    {
+        private readonly HttpClient client = new HttpClient();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtContent.Text = string.Empty;
+        }
+
+        private async void btnViewHTML_Click(object sender, RoutedEventArgs e)
+        {
+            string uri = txtURL.Text;
+
+            try
+            {
+                string responseBody = await client.GetStringAsync(uri);
+                txtContent.Text = responseBody.Trim();
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show($"Message: {ex.Message}");
+            }
+        }
+    }
+}
